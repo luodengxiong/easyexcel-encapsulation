3b9d6b174ede0dc54b1b501ac3989bc1d03e81c9
LuoDengXiong <55217242@qq.com>
2019-06-21 14:49:49 +0800

    Java8日期时间工具类

--- src/main/java/com/howie/easyexcelmethodencapsulation/excel/TimeUtil.java
+++ src/main/java/com/howie/easyexcelmethodencapsulation/excel/TimeUtil.java
@@ -1,9 +1,156 @@
 package com.howie.easyexcelmethodencapsulation.excel;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
 /**
- * @desc: ${description}
- * @author: luodengxiong
- * @date: 2019/6/21 14:01:21
+ * @desc: 基于Java8的时间日期工具类
  **/
-public class TimeUtil {
+public final class TimeUtil {
+
+    /**
+     * 获取默认时间格式: yyyy-MM-dd HH:mm:ss
+     */
+    private static final DateTimeFormatter DEFAULT_DATETIME_FORMATTER = TimeFormat.LONG_DATE_PATTERN_LINE.formatter;
+
+    private TimeUtil() {
+        // no construct function
+    }
+
+    /**
+     * String 转时间
+     *
+     * @param timeStr
+     * @return
+     */
+    public static LocalDateTime parseTime(String timeStr) {
+        return LocalDateTime.parse(timeStr, DEFAULT_DATETIME_FORMATTER);
+    }
+
+    /**
+     * String 转时间
+     *
+     * @param timeStr
+     * @param format  时间格式
+     * @return
+     */
+    public static LocalDateTime parseTime(String timeStr, TimeFormat format) {
+        return LocalDateTime.parse(timeStr, format.formatter);
+    }
+
+    /**
+     * 时间转 String
+     *
+     * @param time
+     * @return
+     */
+    public static String parseTime(LocalDateTime time) {
+        return DEFAULT_DATETIME_FORMATTER.format(time);
+    }
+
+    /**
+     * 时间转 String
+     *
+     * @param time
+     * @param format 时间格式
+     * @return
+     */
+    public static String parseTime(LocalDateTime time, TimeFormat format) {
+        return format.formatter.format(time);
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @return
+     */
+    public static String getCurrentDatetime() {
+        return DEFAULT_DATETIME_FORMATTER.format(LocalDateTime.now());
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @param format 时间格式
+     * @return
+     */
+    public static String getCurrentDatetime(TimeFormat format) {
+        return format.formatter.format(LocalDateTime.now());
+    }
+
+    /**
+     * 时间格式
+     */
+    public enum TimeFormat {
+
+        /**
+         * 短时间格式
+         */
+        SHORT_DATE_PATTERN_LINE("yyyy-MM-dd"),
+        SHORT_DATE_PATTERN_SLASH("yyyy/MM/dd"),
+        SHORT_DATE_PATTERN_DOUBLE_SLASH("yyyy\\MM\\dd"),
+        SHORT_DATE_PATTERN_NONE("yyyyMMdd"),
+
+        /**
+         * 长时间格式
+         */
+        LONG_DATE_PATTERN_LINE("yyyy-MM-dd HH:mm:ss"),
+        LONG_DATE_PATTERN_SLASH("yyyy/MM/dd HH:mm:ss"),
+        LONG_DATE_PATTERN_DOUBLE_SLASH("yyyy\\MM\\dd HH:mm:ss"),
+        LONG_DATE_PATTERN_NONE("yyyyMMdd HH:mm:ss"),
+
+        /**
+         * 长时间格式 带毫秒
+         */
+        LONG_DATE_PATTERN_WITH_MILSEC_LINE("yyyy-MM-dd HH:mm:ss.SSS"),
+        LONG_DATE_PATTERN_WITH_MILSEC_SLASH("yyyy/MM/dd HH:mm:ss.SSS"),
+        LONG_DATE_PATTERN_WITH_MILSEC_DOUBLE_SLASH("yyyy\\MM\\dd HH:mm:ss.SSS"),
+        LONG_DATE_PATTERN_WITH_MILSEC_NONE("yyyyMMdd HH:mm:ss.SSS");
+
+        private transient DateTimeFormatter formatter;
+
+        TimeFormat(String pattern) {
+            formatter = DateTimeFormatter.ofPattern(pattern);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        //获取当前时间
+        LocalDate inputDate = LocalDate.now();
+
+        //当天开始
+        LocalDateTime today_start = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+        //当天结束
+        LocalDateTime today_end = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
+        String todayStartTime = today_start.format(DEFAULT_DATETIME_FORMATTER);
+        String todayEndTime = today_end.format(DEFAULT_DATETIME_FORMATTER);
+        System.out.println("todayStartTime:"+todayStartTime);
+        System.out.println("todayEndTime:"+todayEndTime);
+
+        //本周开始时间
+        TemporalAdjuster FIRST_OF_WEEK =
+                TemporalAdjusters.ofDateAdjuster(localDate -> localDate.minusDays(localDate.getDayOfWeek().getValue() - DayOfWeek.MONDAY.getValue()));
+        String weekStart = df.format(inputDate.with(FIRST_OF_WEEK));
+        System.out.println("weekStart:"+weekStart);
+        //本周结束时间
+        TemporalAdjuster LAST_OF_WEEK =
+                TemporalAdjusters.ofDateAdjuster(localDate -> localDate.plusDays(DayOfWeek.SUNDAY.getValue() - localDate.getDayOfWeek().getValue()));
+        String weekEnd = df.format(inputDate.with(LAST_OF_WEEK));
+        System.out.println("weekEnd:"+weekEnd);
+
+        //本月的第一天
+        String monthStart = df.format(LocalDate.of(inputDate.getYear(), inputDate.getMonth(), 1));
+        System.out.println("monthStart:"+monthStart);
+        //本月的最后一天
+        String monthEnd = df.format(inputDate.with(TemporalAdjusters.lastDayOfMonth()));
+        System.out.println("monthEnd:"+monthEnd);
+    }
+
 }
--- src/test/java/com/howie/easyexcelmethodencapsulation/GetFilesTest.java
+++ src/test/java/com/howie/easyexcelmethodencapsulation/GetFilesTest.java
@@ -1,9 +1,47 @@
 package com.howie.easyexcelmethodencapsulation;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
 /**
- * @desc: ${description}
- * @author: luodengxiong
- * @date: 2019/6/21 8:54:35
+ * @desc: 读取文件
  **/
 public class GetFilesTest {
+
+    @Test
+    public void getFile(){
+        String path="D:\\temp";
+        File tfsFileRoot=new File(path);
+        traverseFolder(tfsFileRoot);
+    }
+
+    public void traverseFolder(File tfsFileRoot){
+        File[] tfsFiles=tfsFileRoot.listFiles();
+        for (int i=0;i<tfsFiles.length;i++){
+            File tfsFile=tfsFiles[i];
+            if (!tfsFile.isHidden()) {
+                if (tfsFile.isDirectory()) {
+                    traverseFolder(tfsFile);
+                } else {
+                    Instant instant = Instant.ofEpochMilli(tfsFile.lastModified());
+                    LocalDateTime fileLastModified=LocalDateTime.ofInstant(instant,ZoneId.systemDefault());
+                    LocalDate inputDate = LocalDate.now();
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    //本周开始时间
+                    TemporalAdjuster FIRST_OF_WEEK =
+                            TemporalAdjusters.ofDateAdjuster(localDate -> localDate.minusDays(localDate.getDayOfWeek().getValue() - DayOfWeek.MONDAY.getValue()));
+                    if (fileLastModified.isAfter(LocalDateTime.of(inputDate.with(FIRST_OF_WEEK),LocalTime.of(18, 30)))) {
+                        System.out.println("文档地址: " + tfsFile.getPath());
+                    }
+                }
+            }
+        }
+    }
+
+
 }
